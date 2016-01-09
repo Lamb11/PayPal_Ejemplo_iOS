0d3a111b361330801e4bbfabb4356ede85b81f84
Albert <abc_1118@hotmail.com>
2016-01-09 00:28:56 -0600

    Librerías de PayPal Ademas de Integrar el IDE de PayPal en AppDelegate

--- PayPal_Ejemplo_iOS/AppDelegate.m
+++ PayPal_Ejemplo_iOS/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "PayPalMobile.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    // ...
+    [PayPalMobile initializeWithClientIdsForEnvironments:@{PayPalEnvironmentProduction : @"YOUR_CLIENT_ID_FOR_PRODUCTION",
+                                                           PayPalEnvironmentSandbox : @"AfcCEAdyEisKqDrJVrWxnphTI41BhyW5XEmKF1pMXafoawXUaekCRpNKHKnrfSCAY4SJkoYUrMiDrN"}];
+    // ...
     return YES;
 }
 
